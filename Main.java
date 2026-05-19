030eddab97875ef84a0935c1c89d8fb2993a0617
avagamble <ava.gamble37@gmail.com>
2026-05-19 04:27:07 +0000

    completed lab

--- Main.java
+++ Main.java
@@ -28,19 +28,34 @@ Yearly Global Atmospheric CO₂ Levels (parts per million)
 
 class Main 
 {
-	//CONSTANTS SECTION
+  //CONSTANTS SECTION
 
   public static void main(String[] args) 
   {
-		// DECLARATION SECTION
+    // DECLARATION SECTION
+    double[] co2Levels;
+    int[] years;
 
-		// INITIALIZATION SECTION
+    // INITIALIZATION SECTION
+    co2Levels = new double[]{
+      371.32, 373.45, 375.98, 377.70, 379.98, 
+      382.09, 384.03, 385.83, 387.64, 390.10, 
+      391.85, 394.06, 396.74, 398.87, 401.01, 
+      404.41, 406.76, 408.72, 411.66, 414.24
+    };
 
-		// INPUT SECTION 
+    years = new int[20];
+    int startYear = 2001;
+    for (int i = 0; i < years.length; i++) {
+      years[i] = startYear;
+      startYear++;
+    }
 
-		// PROCESSING SECTION
+    // INPUT SECTION 
 
-		// OUTPUT SECTION
+    // PROCESSING SECTION
+
+    // OUTPUT SECTION
   
     //Explanation of oil drum units
     System.out.println("\nData displayed for each year begins at 360 ppm.\nEach additional oil drum (🛢 ) represents an additional 10 ppm.\n");
@@ -48,7 +63,26 @@ class Main
     //Column titles of graph
     System.out.print("Year");
     UtilityBelt.printCentered(55,"CO₂ in Atmosphere (ppm)");
+    System.out.println();
 
+    printGraph(co2Levels, years);
+
+    double difference = co2Levels[co2Levels.length - 1] - co2Levels[0];
+    System.out.printf("From 2001 to 2020, the average atmospheric CO₂ levels across the globe has grown %.2f ppm.\n", difference);
   }
 
+  public static void printBar(double co2Value) {
+    int drums = (int) (co2Value - 360);
+    for (int i = 0; i < drums; i++) {
+      System.out.print("🛢");
+    }
+    System.out.println(" " + co2Value);
+  }
+
+  public static void printGraph(double[] co2Levels, int[] years) {
+    for (int i = 0; i < co2Levels.length; i++) {
+      System.out.print(years[i] + " ");
+      printBar(co2Levels[i]);
+    }
+  }
 }
